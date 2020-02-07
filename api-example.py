9e3bc47d536f12c9e98a2ee51fcebf7e8b8ced09
senticrypt <47682874+senticrypt@users.noreply.github.com>
2020-02-07 11:43:56 -0800

    Update api-example.py

--- api-example.py
+++ api-example.py
@@ -1,11 +1,10 @@
-import json
 import requests
 
 # Example of querying the API
 result = requests.get('https://api.senticrypt.com/v1/bitcoin.json')
 result = result.json()
-print('Oldest result: {}'.format(result[0])
-print('Newest result: {}'.format(result[-1])
+print('Oldest result: {}'.format(result[0]))
+print('Newest result: {}'.format(result[-1]))
 
 # Example calculations
 mean_times_rate = result[-1]['rate'] * results[-1]['mean']
