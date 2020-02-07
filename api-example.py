913f7e0f2d1511c9160fd375d59c7803c3eaa0f0
senticrypt <47682874+senticrypt@users.noreply.github.com>
2020-02-07 11:39:47 -0800

    Create api-example.py

---
+++ api-example.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+# Example of querying the API
+result = requests.get('https://api.senticrypt.com/v1/bitcoin.json')
+result = result.json()
+print('Oldest result: {}'.format(result[0])
+print('Newest result: {}'.format(result[-1])
+
+# Example calculations
+mean_times_rate = result[-1]['rate'] * results[-1]['mean']
+median_times_rate = result[-1]['rate'] * results[-1]['median']
+
+if median_times_rate > mean_times_rate:
+  print('BUY')
+  
